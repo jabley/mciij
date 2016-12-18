18e48fbc436503f608f3f6c3d74bae0f91388a2e
James Abley <james.abley@gmail.com>
2016-12-18 16:26:01 +0000

    Add classes from the text
    - Table
    - IntAndTable

--- tiger/chap1/interp.java
+++ tiger/chap1/interp.java
@@ -46,3 +46,34 @@ class interp {
     interp(prog.prog);
   }
 }
+
+class Table {
+  String id;
+  int value;
+  Table tail;
+
+  Table(String id, int value, Table tail) {
+    this.id = id;
+    this.value = value;
+    this.tail = tail;
+  }
+
+  int lookup(String key) {
+    if (this.id.equals(key)) {
+      return this.value;
+    }
+    if (this.tail != null) {
+      return this.tail.lookup(key);
+    }
+    throw new IllegalStateException("Variable " + key + " not found in table");
+  }
+}
+
+class IntAndTable {
+  int i;
+  Table t;
+  IntAndTable(int i, Table t) {
+    this.i = i;
+    this.t = t;
+  }
+}
