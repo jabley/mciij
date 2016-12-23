5a00f37c973a2f5b60367c921dd0563428500ac4
James Abley <james.abley@gmail.com>
2016-12-23 08:57:45 +0000

    Format how Java is typically formatted
    Pattern-matching is important.

--- tiger/chap1/src/main/java/slp.java
+++ tiger/chap1/src/main/java/slp.java
@@ -1,51 +1,81 @@
 abstract class Stm {}
 
 class CompoundStm extends Stm {
-   Stm stm1, stm2;
-   CompoundStm(Stm s1, Stm s2) {stm1=s1; stm2=s2;}
+   Stm stm1;
+   Stm stm2;
+   CompoundStm(Stm s1, Stm s2) {
+     stm1=s1;
+     stm2=s2;
+   }
 }
 
 class AssignStm extends Stm {
-   String id; Exp exp;
-   AssignStm(String i, Exp e) {id=i; exp=e;}
+   String id;
+   Exp exp;
+   AssignStm(String i, Exp e) {
+     id=i;
+     exp=e;
+   }
 }
 
 class PrintStm extends Stm {
    ExpList exps;
-   PrintStm(ExpList e) {exps=e;}
+   PrintStm(ExpList e) {
+     exps=e;
+   }
 }
 
 abstract class Exp {}
 
 class IdExp extends Exp {
    String id;
-   IdExp(String i) {id=i;}
+   IdExp(String i) {
+     id=i;
+   }
 }
 
 class NumExp extends Exp {
    int num;
-   NumExp(int n) {num=n;}
+   NumExp(int n) {
+     num=n;
+   }
 }
 
 class OpExp extends Exp {
-   Exp left, right; int oper;
+   Exp left;
+   Exp right;
+   int oper;
    final static int Plus=1,Minus=2,Times=3,Div=4;
-   OpExp(Exp l, int o, Exp r) {left=l; oper=o; right=r;}
+   OpExp(Exp l, int o, Exp r) {
+     left=l;
+     oper=o;
+     right=r;
+   }
 }
 
 class EseqExp extends Exp {
-   Stm stm; Exp exp;
-   EseqExp(Stm s, Exp e) {stm=s; exp=e;}
+   Stm stm;
+   Exp exp;
+   EseqExp(Stm s, Exp e) {
+     stm=s;
+     exp=e;
+   }
 }
 
 abstract class ExpList {}
 
 class PairExpList extends ExpList {
-   Exp head; ExpList tail;
-   public PairExpList(Exp h, ExpList t) {head=h; tail=t;}
+   Exp head;
+   ExpList tail;
+   public PairExpList(Exp h, ExpList t) {
+     head=h;
+     tail=t;
+   }
 }
 
 class LastExpList extends ExpList {
-   Exp head; 
-   public LastExpList(Exp h) {head=h;}
+   Exp head;
+   public LastExpList(Exp h) {
+     head=h;
+   }
 }
