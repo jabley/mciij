9f3488fe847a19686c466401545a0dd2bd191f8a
James Abley <james.abley@gmail.com>
2016-12-29 09:42:09 +0000

    Exercise 1.1 part c
    - Add a method to create a Tree from a string
    
    Treat it as keys, with the values being the position in the string.
    Taken from Sedgewick [1]
    
    - Add a method to calculate the height of a Tree
    
    Using this as a high-level indicator for whether a Tree is balanced. We
    can see that the Tree from "abcdefghi" is not currently balanced, since
    it has a height of 9. That means that it is very one-sided.
    
    [1] https://www.amazon.co.uk/gp/product/B004P8J1NA/

--- tiger/chap1/src/main/java/Tree.java
+++ tiger/chap1/src/main/java/Tree.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Tree {
   Tree left;
   String key;
@@ -53,6 +55,38 @@ public class Tree {
     throw new IllegalStateException("Not sure how to compare the key");
   }
 
+  static Tree treeFromString(String input) {
+    if (input == null) {
+      throw new IllegalArgumentException("input == null");
+    }
+    return treeFromChars(input.toCharArray());
+  }
+
+  static Tree treeFromChars(char[] chars) {
+    if (chars.length == 0) {
+      return insert("", 0, null);
+    }
+    return treeFromChars(null, 0, chars);
+  }
+
+  static Tree treeFromChars(Tree t, int pos, char[] chars) {
+    String head = String.valueOf(chars[0]);
+    Tree newTree = insert(head, pos, t);
+    if (chars.length == 1) {
+      return newTree;
+    }
+    return treeFromChars(newTree, pos + 1, Arrays.copyOfRange(chars, 1, chars.length));
+  }
+
+  static int height(Tree t) {
+    if (t == null) {
+      return 0;
+    }
+    int left = height(t.left);
+    int right = height(t.right);
+    return 1 + Math.max(left, right);
+  }
+
   public static void main(String[] args) {
     Tree tree1 = insert("a", 1, null);
     Tree tree2 = insert("b", 2, tree1);
@@ -60,11 +94,14 @@ public class Tree {
     System.out.println(String.format("b is in Tree2? %b", member("b", tree2)));
     System.out.println(String.format("a is in Tree2? %b", member("a", tree2)));
     System.out.println(String.format("b is in Tree1? %b", member("b", tree1)));
-
+    System.out.println();
     System.out.println(String.format("Value of c in Tree2? %s", lookup("c", tree2)));
     System.out.println(String.format("Value of b in Tree2? %s", lookup("b", tree2)));
     System.out.println(String.format("Value of a in Tree2? %s", lookup("a", tree2)));
     System.out.println(String.format("Value of b in Tree1? %s", lookup("b", tree1)));
+    System.out.println();
+    System.out.println(String.format("Height of tspipfbst: %s", height(treeFromString("tspipfbst"))));
+    System.out.println(String.format("Height of abcdefghi: %s", height(treeFromString("abcdefghi"))));
   }
 
 }
